c936e57e9766910537090f3d49ca50ef361c4de4
Marlon José López Meza <malm.gm@gmail.com>
2022-04-24 21:46:26 -0500

    Updating categories list for standard fields json output

--- src/main/javascript/routes/categories.route.js
+++ src/main/javascript/routes/categories.route.js
@@ -3,20 +3,56 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
+const jsonPath = path.join(process.cwd(), 'src', 'main', 'json', 'food_categories.data.json');
+
 /* defines home rout for displaying categories list */
 router.get('/', (req, res) => {
-  const jsonPath= path.join(process.cwd(),'src','main','json','food_categories.data.json');
-  fs.readFile(jsonPath,'utf-8', (err, data) => {
+  fs.readFile(jsonPath, 'utf-8', (err, data) => {
     const rootUrl = `${req.protocol}://${req.get('Host')}`;
-    if (err) { 
-      res.status(500).json({ error : 'no categories list' }); 
+    if (err) {
+      res.status(500).json({
+        error: 'no categories list'
+      });
     }
-    res.set('Content-Type','application/json');
-    res.status(200).json(
-      JSON.parse(data).map(
-        (row,pos) => Object.assign({ id: pos+1},row, {picture: `${rootUrl}${row['picture']}` }))
+    res.status(200).json({
+      results: JSON.parse(data).map(
+        (row, pos) => Object.assign({
+          id: pos + 1
+        }, row, {
+          picture: `${rootUrl}${row['picture']}`
+        }))
+    });
+  });
+});
+
+router.get('/:category_id', (req, res) => {
+  fs.readFile(jsonPath, 'utf-8', (err, data) => {
+    const rootUrl = `${req.protocol}://${req.get('Host')}`;
+    if (err) {
+      res.status(500).json({
+        error: 'no categories to search'
+      });
+    }
+    const singleCategory = JSON.parse(data).map(
+      (row, pos) => Object.assign({
+        id: pos + 1
+      }, row, {
+        picture: `${rootUrl}${row['picture']}`
+      })
+    ).find(
+      itm => itm.id === Number(req.params.category_id)
     );
+    if (!singleCategory) {
+      res.status(500).json({
+        error: 'searched category does not exist'
+      });
+    } else {
+      res.status(200).json({
+        result: singleCategory
+      });
+    }
   });
 });
 
 module.exports = router;
+
